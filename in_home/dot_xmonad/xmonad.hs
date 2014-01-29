270cc0da3e17fe63ea2c5cba2588f92b7e51da42
Cyril <cyril.brouillard@gmail.com>
2014-01-29 11:29:02 +0100

    Mise à jour janvier 2014.

--- in_home/dot_xmonad/xmonad.hs
+++ in_home/dot_xmonad/xmonad.hs
@@ -1,4 +1,6 @@
 import qualified Data.Map as M 
+import qualified XMonad.StackSet as W
+
 import System.IO
 import XMonad
 import XMonad.Actions.CycleWS
@@ -28,35 +30,55 @@ myManageHook = composeAll
     , resource  =? "Do"   --> doIgnore
     ]
 
-myWorkspaces  = ["1:www","2:dev","3:server","4:doc","5","6", "7", "8", "9:chat"]
+myWorkspaces  = ["1:www","2:dev","3:exec","4:db","5","6", "7", "8", "9","10", "11", "12:chat"]
 
 newKeys x = M.union (M.fromList (myKeys x)) (keys azertyConfig x)
 myKeys conf@(XConfig {XMonad.modMask = modMask}) =
     [ ((modMask .|. shiftMask, xK_l), spawn "gnome-screensaver-command --lock")
     , ((modMask, xK_quoteleft), spawn "rotatexkbmap") -- with qwerty keyboard
     , ((modMask, xK_twosuperior), spawn "rotatexkbmap") -- with azerty keyboard  
-    , ((modMask, xK_p), spawn "exe=`dmenu_run -nb black -nf yellow -sf yellow` && eval \"exec $exe\"") 
+    , ((modMask, xK_p), spawn "exe=`dmenu_run -nb black -nf yellow -sf yellow` && eval \"exec $exe\"")
+    , ((0,0x1008FF11), spawn "pactl set-sink-volume 1 -- -1.5%")
+    , ((0,0x1008FF13), spawn "pactl set-sink-volume 1 +1.5%")
+    , ((0,0x1008FF12), spawn "pactl set-sink-mute 1 toggle")
+    , ((0,0x1008FF02), spawn "xbacklight -inc 5")
+    , ((0,0x1008FF03), spawn "xbacklight -dec 5") 
 	]
+    ++ 	
+    --
+    -- mod-[1..9], Switch to workspace N
+    -- mod-shift-[1..9], Move client to workspace N
+    --
+    [((m .|. modMask, k), windows $ f i)
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_1, xK_2, xK_3, xK_4, xK_5, xK_6, xK_7, xK_8, xK_9, xK_agrave, xK_parenright, xK_equal]
+        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
 
 -- Layouts
 myLayout = (toggleLayouts $ noBorders Full) $ -- toggle fullscreen
   (layoutHook defaultConfig ||| noBorders tabbed ||| Grid)
   where tabbed = named "Tabbed" $ simpleTabbed
 
+myFocusedBorderColor = "#FFF"
+myBorderColor = "#555"
+myBorderWidth = 2
+
 main = do
       xmproc <- spawnPipe "xmobar"
       xmonad $ withUrgencyHook NoUrgencyHook $ azertyConfig {
-                          --manageHook = myManageHook <+> manageHook defaultConfig,
+                           --manageHook = myManageHook <+> manageHook defaultConfig,
                           manageHook = manageDocks <+> manageHook defaultConfig,
                           keys = newKeys,
                           modMask    = mod4Mask,
                           workspaces = myWorkspaces,
                           layoutHook = smartBorders (avoidStruts $ myLayout),
                           startupHook = setWMName "LG3D",
+			  focusedBorderColor = myFocusedBorderColor,
+			  normalBorderColor = myBorderColor,
+			  borderWidth = myBorderWidth,
                           terminal = "terminator",
                           logHook = (dynamicLogWithPP $ xmobarPP
                                     { ppOutput = hPutStrLn xmproc
-                                    , ppCurrent = xmobarColor "#09F" "" . wrap "[" "]"
+                                    , ppCurrent = xmobarColor "#19F" "" . wrap "[" "]"
                                     , ppTitle = xmobarColor "pink" "" . shorten 50
                                     }) >> updatePointer (Relative 0.5 0.5) >> takeTopFocus  
 }
